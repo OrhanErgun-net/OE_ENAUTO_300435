5df829f0e129030839eb219296bb4724f8464dd4
Sajjad Ghaffoori <sajjad@orhanergun.net>
2022-06-28 20:42:19 +0300

    allnew

--- raw/python_ncclient_oper_example.py
+++
@@ -1,6 +0,0 @@
-from ncclient import manager
-
-with manager.connect(host="192.168.1.211", port=830, username="OEUser01", hostkey_verify=False) as m:
-    c = m.get_config(source='running').data_xml
-    with open("%s.xml" % host, 'w') as f:
-        f.write(c)
